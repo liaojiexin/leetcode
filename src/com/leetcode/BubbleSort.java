20164340cd29f5a66329e890daf01ff9a3f2ccd8
liaojiexin <570839281@qq.com>
2021-06-09 16:25:33 +0800

    算法 2021.6.9 冒泡排序

--- src/com/leetcode/BubbleSort.java
+++ src/com/leetcode/BubbleSort.java
@@ -1,11 +1,11 @@
 package com.leetcode;
 
 /*
-冒泡排序：每次遍历最大或最小的数字，把最大或最小的数字放到最后或最前面
+冒泡排序：每次遍历找出最大或最小的数字，把最大或最小的数字放到最后或最前面
 * */
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] array=new int[]{1,6,2,6,3,7,9,1,5};
+        int[] array=new int[]{1,6,2,6,3,4,7,9,1,5};
         for(int i=0;i<array.length;i++){
             for (int j=i+1;j<array.length;j++){
                 if (array[i]>array[j])
